25262b70317632aff1bdffd09795cffb36a0dd99
Joshua Knauber <joshua.knauber@gmail.com>
2026-03-13 15:45:54 +0100

    docs

---
+++ apps/docs/src/app/layout.tsx
@@ -0,0 +1,48 @@
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import type { Metadata } from 'next';
+import './global.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist',
+});
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: 'breadcrumb',
+    template: '%s | breadcrumb',
+  },
+  description: 'Documentation for breadcrumb.',
+  applicationName: 'breadcrumb',
+};
+
+export default function Layout({ children }: LayoutProps<'/'>) {
+  return (
+    <html lang="en" className={`${geist.variable} ${geistMono.variable}`} suppressHydrationWarning>
+      <head>
+        <script
+          defer
+          data-domain="breadcrumb.sh"
+          src="https://nudge-events.up.railway.app/js/script.js"
+        />
+      </head>
+      <body className={`${geist.className} flex flex-col min-h-screen`}>
+        <RootProvider
+          theme={{
+            forcedTheme: 'dark',
+            defaultTheme: 'dark',
+            enableSystem: false,
+          }}
+        >
+          {children}
+        </RootProvider>
+      </body>
+    </html>
+  );
+}
