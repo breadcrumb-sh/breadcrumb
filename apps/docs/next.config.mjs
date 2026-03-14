a6cca8a98a0deb33ef8ec8db6ed85878bde07684
Joshua Knauber <joshua.knauber@gmail.com>
2026-03-14 18:48:43 +0100

    docs sections

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -3,11 +3,15 @@ import { createMDX } from 'fumadocs-mdx/next';
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
+
 const config = {
   reactStrictMode: true,
-  output: 'export',
-  assetPrefix: '/docs',
-  trailingSlash: true,
+  ...(!isDev && {
+    output: 'export',
+    assetPrefix: '/docs',
+    trailingSlash: true,
+  }),
 };
 
 export default withMDX(config);
