124442020335c4eabbf2fb16ab44279bf0bf1a00
Joshua Knauber <joshua.knauber@gmail.com>
2026-03-15 10:06:13 +0100

    test fix

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -2,7 +2,7 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { Metadata } from 'next';
 import './global.css';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { Banner } from 'fumadocs-ui/components/banner';
+
 
 const geist = Geist({
   subsets: ['latin'],
@@ -42,7 +42,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
           defer
           data-domain="breadcrumb.sh"
           src="https://nudge-events.up.railway.app/js/script.js"
-        />
+        ></script>
       </head>
       <body className={`${geist.className} flex flex-col min-h-screen`}>
         <RootProvider
@@ -52,7 +52,6 @@ export default function Layout({ children }: LayoutProps<'/'>) {
             enableSystem: false,
           }}
         >
-          <Banner>Breadcrumb is now in beta!&nbsp;-&nbsp;<a href="https://github.com/joshuaKnauber/breadcrumb" target="_blank" rel="noopener noreferrer" className="underline">Star us on GitHub</a></Banner>
           {children}
         </RootProvider>
       </body>
